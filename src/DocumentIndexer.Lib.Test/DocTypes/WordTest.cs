285e7a0b9fb16a96ef9f25ffeb174b6788179f42
Wm. Barrett Simms <barrett@wbsimms.com>
2015-03-28 10:52:24 -0400

    Add word parsing

---
+++ src/DocumentIndexer.Lib.Test/DocTypes/WordTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DocumentIndexer.Lib.DocTypes;
+using Microsoft.Practices.Unity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DocumentIndexer.Lib.Test.DocTypes
+{
+	[TestClass]
+	public class WordTest
+	{
+		[TestMethod]
+		public void ConstructorTest()
+		{
+			var word = Resolver.Instance.Container.Resolve<IWord>();
+			Assert.IsNotNull(word);
+		}
+
+		[TestMethod]
+		public void ExecuteTest()
+		{
+			var word = Resolver.Instance.Container.Resolve<IWord>();
+			IndexModel model = new IndexModel() { FileName = @"C:\Projects\eDesktop\TestData\TestWord.docx" };
+			var result = word.Execute(model);
+			Assert.IsNotNull(result);
+			Assert.IsFalse(result.HasError);
+
+		}
+	}
+}
---
+++ src/DocumentIndexer.Lib/IndexModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentIndexer.Lib
+{
+	public class IndexModel
+	{
+		public string FileName { get; set; }
+		public string Id { get; set; }
+	}
+}
